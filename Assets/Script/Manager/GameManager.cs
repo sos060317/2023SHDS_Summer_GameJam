96721e6e7d0ed71b5428e95b672702f8090117b8
WooJiWon <woojiwon060317@gmail.com>
2023-07-22 14:24:23 +0900

    추가: 게임 매니저 스크립트

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
