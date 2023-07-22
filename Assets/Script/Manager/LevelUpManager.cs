273d0444d4e7adffdbc22d075cc96ff616ca6e90
WooJiWon <woojiwon060317@gmail.com>
2023-07-23 03:45:54 +0900

    추가: 레벨업 카드 내려오게 하기

--- Assets/Script/Manager/LevelUpManager.cs
+++ Assets/Script/Manager/LevelUpManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelUpManager : MonoBehaviour
@@ -16,6 +18,14 @@ public class LevelUpManager : MonoBehaviour
             return instance;
         }
     }
+
+    [SerializeField] private List<GameObject> abilityCard = new List<GameObject>();
+    [SerializeField] private Transform[] cardPosition;
+    [SerializeField] private Transform[] cardFirstPosition;
+
+    public Canvas canvas;
+    
+    //private int arrNumber = 0;
 
     private void Awake()
     {
@@ -43,6 +53,40 @@ public class LevelUpManager : MonoBehaviour
 
     public void ChoiceAbility()
     {
-        Debug.Log("adf");
+        List<GameObject> choiceCard = new List<GameObject>(abilityCard);
+
+        GameObject[] choiceAbiliey = new GameObject[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            int choiceNumber = Random.RandomRange(0, choiceCard.Count);
+
+            choiceAbiliey[i] = choiceCard[choiceNumber];
+            
+            var card = Instantiate(choiceAbiliey[i], cardPosition[i].position, Quaternion.identity, canvas.transform);
+
+            StartCoroutine(DownCard(card, cardPosition[i], i));
+
+            choiceCard.RemoveAt(choiceNumber);
+        }
+    }
+
+    IEnumerator DownCard(GameObject abilityCard, Transform firstPosition, int num)
+    {
+        float count = 0;
+        
+        while (count <= 1f)
+        {
+            count += Time.deltaTime;
+            abilityCard.transform.position = Vector3.Lerp(firstPosition.position, cardFirstPosition[num].position, count);
+            yield return null;
+        }
+        
+        while (count >= 0.7f)
+        {
+            count -= Time.deltaTime;
+            abilityCard.transform.position = Vector3.Lerp(firstPosition.position, cardFirstPosition[num].position, count);
+            yield return null;
+        }
     }
 }
