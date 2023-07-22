91dfe6ac594bbdb3bca9e01cfcd421681e9543a1
WooJiWon <woojiwon060317@gmail.com>
2023-07-22 22:12:13 +0900

    추가: Level Up System 능력 선택

---
+++ Assets/Script/Manager/LevelUpManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class LevelUpManager : MonoBehaviour
+{
+    public static LevelUpManager instance = null;
+    
+    public static LevelUpManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChoiceAbility()
+    {
+        Debug.Log("adf");
+    }
+}
--- Assets/Script/Player/WooJiWon/GaugeManager.cs
+++ Assets/Script/Player/WooJiWon/GaugeManager.cs
@@ -1,15 +1,37 @@
+using System;
 using UnityEngine;
 
 public class GaugeManager : MonoBehaviour
 {
-    public static GaugeManager Instance;
+    public static GaugeManager instance = null;
+    
+    public static GaugeManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
+    
     public PlayerExp expGauge;
     public PlayerExp hpGauge;
     public PlayerExp manaGauge;
 
     private void Awake()
     {
-        Instance = this;
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Start()
@@ -19,5 +41,19 @@ public class GaugeManager : MonoBehaviour
         hpGauge.SetValue(100);
         
         manaGauge.SetValue(100);
+    }
+
+    private void Update()
+    {
+        CheckExp();
+    }
+
+    private void CheckExp()
+    {
+        if (expGauge.currentValue >= expGauge.GetComponent<PlayerExp>().maxValue)
+        {
+            LevelUpManager.Instance.ChoiceAbility();
+            expGauge.currentValue = 0;
+        }
     }
 }
