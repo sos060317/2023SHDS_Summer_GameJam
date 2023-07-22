cae80a7e6af02725b23adf4705800918547d96a9
AhnSeonghun <888seonghun888@gmail.com>
2023-07-22 17:27:03 +0900

    수정: 적 대시

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -9,9 +9,10 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float E_dash_Delay;
     private float Cur_E_dash_Delay = 0;
     private Transform player;
+    private Transform player_fix;
+    
     
     Rigidbody rigid;
-
 
     void Start()
     {
@@ -34,7 +35,7 @@ public class Enemy : MonoBehaviour
         transform.position += direction * E_moveSpeed * Time.deltaTime;
     }
 
-    void E_dash_CoolTime() // 대시의 쿨타임이 돌 때마다 몬스터가 대시한다.
+    void E_dash_CoolTime() // 대시의 쿨타임이 돌 때마다 적이 대시한다.
     {
         if(E_dash_Delay > Cur_E_dash_Delay)
         {
@@ -42,18 +43,32 @@ public class Enemy : MonoBehaviour
         }
         else
         {
+            player_fix = player;
+            rotate();
             StartCoroutine(E_dash());
         }
     }
 
-    IEnumerator E_dash() // 몬스터 대시
+    IEnumerator E_dash() // 적 대시
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.15f);
 
-        Vector3 direction = player.position - transform.position;
-        direction.Normalize();
+        //Vector3 direction = player.position - transform.position;
+        //direction.Normalize();
+        
+        transform.Translate(Vector3.forward * E_BoosterSpeed);
 
-        transform.position += direction * E_BoosterSpeed * Time.deltaTime;
+        //transform.position += direction * E_BoosterSpeed * Time.deltaTime;
         Cur_E_dash_Delay = 0;
+    }
+
+    void rotate() // 플레이어를 바라보는 함수
+    {
+        Vector3 dir = player_fix.transform.position - transform.position;
+        dir.y = 0f;
+
+        Quaternion rot = Quaternion.LookRotation(dir.normalized);
+
+        transform.rotation = rot;
     }
 }
